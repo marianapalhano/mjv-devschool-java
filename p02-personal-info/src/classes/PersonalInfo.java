cfa233253df6ea1deeaaef1f31f67a4830c1256f
Mariana <marianaufc@gmail.com>
2021-12-21 19:31:47 -0300

    p02(fix): fixes Person data

--- p02-personal-info/src/classes/PersonalInfo.java
+++ p02-personal-info/src/classes/PersonalInfo.java
@@ -3,7 +3,7 @@ package classes;
 public class PersonalInfo {
 
 	public static void main(String[] args) {
-		Person mariana = new Person("Mariana Palhano", 40, "marianaufc@gmail.com", "Fortaleza", "649310993-00", "Female", "(85) 99912-9772", "Married", "29/10/1981");
+		Person mariana = new Person("Mariana Palhano", 40, "mariana@gmail.com", "Fortaleza", "999999999-00", "Female", "(85) 99999-9999", "Married", "02/12/1981");
 		mariana.printPerson();
 
 	}
